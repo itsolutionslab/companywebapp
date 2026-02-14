72b25a108e5b9030b2e2b3df0cafec6e56054d3e
EmelCeledonio <alwayscreh@gmail.com>
2026-02-13 23:05:53 -0500

    deploy 27

--- app/[region]/layout.tsx
+++ app/[region]/layout.tsx
@@ -12,6 +12,11 @@ export default async function RegionalLayout({
 
     return (
         <html lang={lang}>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+            </head>
             <body className="antialiased">
                 <RegionProvider initialRegion={region}>
                     <main>
